c86235d5d943bcfabeed7459a57f33ee590b6ad3
apurvkhare <apurvkhare1@gmail.com>
2021-01-17 18:52:37 +0530

    story images and widget updated

--- src/components/StoryReel/StoryReel.tsx
+++ src/components/StoryReel/StoryReel.tsx
@@ -6,32 +6,32 @@ const StoryReel = () => {
     return (
         <div className='storyReel'>
             <Story
-                image='https://picsum.photos/200/300'
-                profileSrc='https://picsum.photos/200/300'
-                title='Ross'
+                image='https://images.immediate.co.uk/production/volatile/sites/3/2018/05/IRON-2008-d7a2706.jpg?quality=90&resize=768,574'
+                profileSrc='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlicYORGJkOifbgyqpA5uweR31pG13Aqh-Pw&usqp=CAU'
+                title='Tony Stark'
             />
 
             <Story
-                image='https://picsum.photos/200/300'
-                profileSrc='https://picsum.photos/200/300'
-                title='Joey'
+                image='https://timesofindia.indiatimes.com/thumb/msid-71273471,width-1200,height-900,resizemode-4/.jpg'
+                profileSrc='https://d2j1wkp1bavyfs.cloudfront.net/wp-content/legacy/posts/67428683-c017-41ac-8fdf-c41fdcb39a0c.jpg'
+                title='Steve Rogers'
             />
 
             <Story
-                image='https://picsum.photos/200/300'
-                profileSrc='https://picsum.photos/200/300'
-                title='Chandler'
+                image='https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/05/14/Pictures/_eac2484e-95c6-11ea-a968-2ed746e9a968.jpg'
+                profileSrc='https://i.pinimg.com/originals/a3/f2/a3/a3f2a3112b94c39e4fb6a50d2295963c.jpg'
+                title='Bruce Banner'
             />
 
             <Story
-                image='https://picsum.photos/200/300'
-                profileSrc='https://picsum.photos/200/300'
-                title='Monica'
+                image='https://static.toiimg.com/photo/msid-75390440/75390440.jpg?344550'
+                profileSrc='https://www.screengeek.net/wp-content/uploads/2019/10/thanos.jpg'
+                title='Thanos'
             />
             <Story
-                image='https://picsum.photos/200/300'
-                profileSrc='https://picsum.photos/200/300'
-                title='Phoebe'
+                image='https://newsorb360.com/wp-content/uploads/2020/04/Thor-Marvel-NewsORB360-822x1024.jpg'
+                profileSrc='https://image.cnbcfm.com/api/v1/image/104819285-thor.jpg?v=1529476684'
+                title='Thor'
             />
         </div>
     )
--- src/components/Widgets/Widgets.tsx
+++ src/components/Widgets/Widgets.tsx
@@ -5,7 +5,7 @@ const Widgets = () => {
     return (
         <div className='widgets'>
             <iframe
-                src='https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&wiidth=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
+                src='https://www.facebook.com/plugins/page.php?href=https://www.facebook.com/MarvelIndia&tabs=timeline&wiidth=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
                 title='Facebook'
                 width='340'
                 height='100%'
